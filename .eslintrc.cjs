9d0ee6f65d95824b2671bf0ac1f85a39b739105b
Burt <1020103647@qq.com>
2023-12-19 16:25:34 +0800

    build(eslint): import/extensions 后缀检测

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -45,6 +45,12 @@ module.exports = {
     'prettier/prettier': 'error',
     // turn on errors for missing imports
     'import/no-unresolved': 'error',
+    // 对后缀的检测，否则 import 一个ts文件也会报错，需要手动添加'.ts', 增加了下面的配置后就不用了
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
   },
   // eslint-import-resolver-typescript 插件，@see https://www.npmjs.com/package/eslint-import-resolver-typescript
   settings: {
