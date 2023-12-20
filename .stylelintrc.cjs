764098d832d1119495b5571a2c27c720aadf16f2
Burt <1020103647@qq.com>
2023-12-20 09:39:36 +0800

    build(stylelint): refine stylelint & add postcss-scss

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
+    'stylelint-config-html/vue',
     'stylelint-config-recess-order',
     'stylelint-config-prettier',
   ],
@@ -15,5 +16,19 @@ module.exports = {
       files: ['**/*.{vue,html}'],
       customSyntax: 'postcss-html',
     },
+    {
+      files: ['**/*.{css,scss}'],
+      customSyntax: 'postcss-scss',
+    },
   ],
+  // 自定义规则
+  rules: {
+    // 允许 global 、export 、v-deep等伪类
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'export', 'v-deep', 'deep'],
+      },
+    ],
+  },
 }
