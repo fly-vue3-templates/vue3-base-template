faf47b74e216afbf2d67c276a30145465823b407
Burt <1020103647@qq.com>
2023-12-20 14:13:49 +0800

    build(vite): add rollup-plugin-visualizer & vite-plugin-restart

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,23 @@ import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import stylelintPlugin from 'vite-plugin-stylelint'
 import svgLoader from 'vite-svg-loader'
+import { visualizer } from 'rollup-plugin-visualizer'
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin(), stylelintPlugin({ fix: true }), svgLoader()],
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    stylelintPlugin({ fix: true }),
+    svgLoader(),
+    // 打包分析插件
+    visualizer(),
+    ViteRestart({
+      // 通过这个插件，在修改vite.config.js文件则不需要重新运行也生效配置
+      restart: ['vite.config.js'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.join(process.cwd(), './src'),
