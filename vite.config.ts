39a1d052f31680f4c0aa00a6d02ffab30542b9c7
Burt <1020103647@qq.com>
2023-12-20 11:19:08 +0800

    build(vite): add vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import stylelintPlugin from 'vite-plugin-stylelint'
-import path from 'node:path'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin(), stylelintPlugin({ fix: true })],
+  plugins: [vue(), eslintPlugin(), stylelintPlugin({ fix: true }), svgLoader()],
   resolve: {
     alias: {
       '@': path.join(process.cwd(), './src'),
