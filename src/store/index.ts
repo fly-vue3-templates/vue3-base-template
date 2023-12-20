2efc22642d608e3bd17477873e272c9f0ff8bf3f
Burt <1020103647@qq.com>
2023-12-20 11:52:18 +0800

    Revert "feat(pinia): 配置持久化的key"
    This reverts commit f70574e45eb93778a48373ca771f504592e9afbd.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
 // src/store/index.ts
 import { createPinia } from 'pinia'
-import { createPersistedState } from 'pinia-plugin-persistedstate' // 数据持久化
+import piniaPersist from 'pinia-plugin-persistedstate' // 数据持久化
 
 const store = createPinia()
-store.use(
-  createPersistedState({
-    key: (id) => `__persisted__${id}`,
-  }),
-)
+store.use(piniaPersist)
 
 export default store
