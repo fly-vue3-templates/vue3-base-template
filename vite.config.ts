363f470f59b2d1b8518395f9446398e2f388fc30
Burt <1020103647@qq.com>
2023-12-20 09:56:42 +0800

    build(vite): add vite-plugin-stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import stylelintPlugin from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
     eslintPlugin({
       include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
     }),
+    stylelintPlugin({ fix: true }),
   ],
 })
