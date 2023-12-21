bbc21e0a463a546f60e2fe0b4413a113c3fd4969
Burt <1020103647@qq.com>
2023-12-21 09:37:32 +0800

    docs(uno.config.ts): add uno 说明文档

--- uno.config.ts
+++ uno.config.ts
@@ -10,10 +10,10 @@ import {
 
 export default defineConfig({
   presets: [
-    presetAttributify({
-      /* preset options */
-    }),
     presetUno(),
+    // 支持css class属性化，eg: `<button bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600" text="sm white">attributify Button</button>`
+    presetAttributify(),
+    // 支持图标，需要搭配图标库，eg: @iconify-json/carbon, 使用 `<button class="i-carbon-sun dark:i-carbon-moon" />`
     presetIcons({
       scale: 1.2,
       warn: true,
@@ -23,5 +23,9 @@ export default defineConfig({
       },
     }),
   ],
-  transformers: [transformerDirectives(), transformerVariantGroup()],
+  transformers: [
+    transformerDirectives(),
+    // 支持css class组合，eg: `<div class="hover:(bg-gray-400 font-medium) font-(light mono)">测试 unocss</div>`
+    transformerVariantGroup(),
+  ],
 })
