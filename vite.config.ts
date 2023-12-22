7208cc4b38060011253420c088cd656222db5e31
Burt <1020103647@qq.com>
2023-12-22 11:41:25 +0800

    chore(vite): refine vite

--- vite.config.ts
+++ vite.config.ts
@@ -22,13 +22,11 @@ import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 import UnoCSS from 'unocss/vite'
 import autoprefixer from 'autoprefixer'
 
-const htmlPlugin = (title: string) => {
+const htmlPlugin = () => {
   return {
     name: 'html-transform',
     transformIndexHtml(html) {
-      return html
-        .replace(/<title>(.*?)<\/title>/, `<title>${title}</title>`)
-        .replace('%BUILD_DATE%', dayjs().format('YYYY-MM-DD HH:mm:ss'))
+      return html.replace('%BUILD_DATE%', dayjs().format('YYYY-MM-DD HH:mm:ss'))
     },
   }
 }
@@ -44,7 +42,7 @@ export default ({ mode }) => {
     plugins: [
       vue(),
       UnoCSS(),
-      htmlPlugin(env.VITE_APP_TITLE),
+      htmlPlugin(),
       eslintPlugin(),
       stylelintPlugin({ fix: true }),
       svgLoader(),
@@ -83,26 +81,21 @@ export default ({ mode }) => {
       // 这个图片压缩插件比较耗时，希望仅在生产环境使用
       viteImagemin({
         gifsicle: {
-          // gif图片压缩
-          optimizationLevel: 3, // 选择1到3之间的优化级别
+          optimizationLevel: 7, // 选择1到10之间的优化级别
           interlaced: false, // 隔行扫描gif进行渐进式渲染
           // colors: 2 // 将每个输出GIF中不同颜色的数量减少到num或更少。数字必须介于2和256之间。
         },
         optipng: {
-          // png
           optimizationLevel: 7, // 选择0到7之间的优化级别
         },
         mozjpeg: {
-          // jpeg
           quality: 20, // 压缩质量，范围从0(最差)到100(最佳)。
         },
         pngquant: {
-          // png
           quality: [0.8, 0.9], // Min和max是介于0(最差)到1(最佳)之间的数字，类似于JPEG。达到或超过最高质量所需的最少量的颜色。如果转换导致质量低于最低质量，图像将不会被保存。
           speed: 4, // 压缩速度，1(强力)到11(最快)
         },
         svgo: {
-          // svg压缩
           plugins: [
             {
               name: 'removeViewBox',
